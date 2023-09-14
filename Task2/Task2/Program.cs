16147d323a637c5518ac902ebc0999aac70b44c4
Fancy <lev2001504@gmail.com>
2023-09-14 17:01:18 +0200

    Осознанное возведение в квадрат

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число для возведени в квадрат:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Квадрат числа = " + number * number);
+            Console.ReadKey();
+        }
+
+
+    }
+}
