c4b33f16e297b7150da370251f0fbe1ff9cc126e
Fancy <lev2001504@gmail.com>
2023-09-14 17:28:49 +0200

    Перестановка О.о

---
+++ Task-3/Task-3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+
+
+            //int number1 = Convert.ToInt32(Console.ReadLine());
+            //int number2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"{ number1 } и { number2 }");
+
+            double number1 = EnterNumber("Число 1");
+            double number2 = EnterNumber("Число 2");
+            double number3 = number1;
+            number1 = number2;
+            number2 = number3;
+            Console.WriteLine($"Значения после перестановки: { number1} {number2}");
+            Console.ReadKey();
+        }
+
+        public static double EnterNumber(string numberName)
+        {
+            Console.WriteLine($"Введите {numberName}:");
+
+            while (true)
+            {
+                string enteredString = Console.ReadLine();
+
+                double result;
+
+                bool isSuccess = double.TryParse(enteredString, out result);// double.TryParse double это как книга в которой есть методы Pars и они неразделимы 
+
+                if (isSuccess)
+                {
+                    return result;
+                }
+
+                Console.WriteLine("Не число!");
+            }
+        }
+    }
+}
