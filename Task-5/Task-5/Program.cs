f2e8672ac2b0ef14798b3b62ae4647d63d238755
Fancy <lev2001504@gmail.com>
2023-09-14 18:13:24 +0200

    Школьники и яблоки, и, на удивление, все пошло по плану

---
+++ Task-5/Task-5/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = EnterNumber("количество школьников");
+            int k = EnterNumber("количество яблок");
+            Console.WriteLine($"Школьники: {n} Яблоки: {k}");
+
+            int result = n / k;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        public static int EnterNumber(string numberName)
+        {
+            Console.WriteLine($"Введите {numberName}:");
+
+            while (true)
+            {
+                string enteredString = Console.ReadLine();
+
+                int result;
+
+                bool isSuccess = int.TryParse(enteredString, out result);
+
+                if (isSuccess)
+                {
+                    return result;
+                }
+
+                Console.WriteLine("Не число!");
+            }
+        }
+    }
+}
