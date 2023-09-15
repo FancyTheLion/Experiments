60e062d1c9e4bb1f8c3ea6ba2393be14e13cc561
Fancy <lev2001504@gmail.com>
2023-09-15 11:38:55 +0200

    Нарисован флаг\треугольник

---
+++ Task-6/Task-6/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task_6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("A");
+                }
+                Console.WriteLine();
+            }
+
+            for (int u = 0; u < 10; u++)
+            {
+                for (int h = 9; h > u; h--)
+                {
+                    Console.Write("A");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        //Console.WriteLine("\tA");
+        //Console.WriteLine("\tAA");
+        //Console.WriteLine("\tAAA");
+        //Console.WriteLine("\tAAAA");
+        //Console.WriteLine("\tAAAAA");
+        //Console.WriteLine("\tAAAAAA");
+        //Console.WriteLine("\tAAAAAAA");
+        //Console.WriteLine("\tAAAAAAAA");
+        //Console.WriteLine("\tAAAAAAAAA");
+        //Console.WriteLine("\tAAAAAAAAAA");
+        //Console.WriteLine("\tAAAAAAAAA");
+        //Console.WriteLine("\tAAAAAAAA");
+        //Console.WriteLine("\tAAAAAAA");
+        //Console.WriteLine("\tAAAAAA");
+        //Console.WriteLine("\tAAAAA");
+        //Console.WriteLine("\tAAAA");
+        //Console.WriteLine("\tAAA");
+        //Console.WriteLine("\tAA");
+        //Console.WriteLine("\tA");
+    }
+}
