3ba18ad54b17a3aeeffc77e5adc33642291e9012
Fancy <lev2001504@gmail.com>
2023-09-15 17:47:04 +0200

    Знакомство с массивом и производство "счастливого билета"

--- Task-8/Task-8/Program.cs
+++ Task-8/Task-8/Program.cs
@@ -2,34 +2,70 @@
 {
     internal class Program
     {
+        private const int ElementsCount = 6;
+
         static void Main(string[] args)
         {
-            int luckyNumber;
-            Console.WriteLine("Введите шестизначное число:");
+            int number = 235640;
+            int numberCopy = number;
 
-            if (!int.TryParse(Console.ReadLine(), out luckyNumber)) ;
+            int[] didgits = new int[ElementsCount];
+
+            for (int i = 0; i < ElementsCount; i++)
             {
-                Console.WriteLine("Введено не число, повторить ввод");
-                return;
+                didgits[i] = number % 10;
+                number = number / 10;
             }
 
-            int luckyNumber1 = luckyNumber / 100000;
-            int luckyNumber2 = luckyNumber / 10000 % 10;
-            int luckyNumber3 = luckyNumber / 1000 % 10;
-            int luckyNumber4 = luckyNumber / 100 % 10;
-            int luckyNumber5 = luckyNumber / 10 % 10;
-            int luckyNumber6 = luckyNumber % 10;
+            int elementsHalfCount = ElementsCount / 2;
 
-            if (luckyNumber1 + luckyNumber2 + luckyNumber3 == luckyNumber4 + luckyNumber5 + luckyNumber6)
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < elementsHalfCount; i++)
             {
-                Console.WriteLine($"Билет с номером {luckyNumber} счастливый!");
+                sum1 += didgits[i];
+                sum2 += didgits[i + elementsHalfCount];
             }
-            Console.WriteLine($"Билет с номером {luckyNumber} несчастливый");
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine($"Билет с номером {numberCopy} счастливый!");
+            }
+            else
+            {
+                Console.WriteLine($"Билет с номером {numberCopy} несчастливый.");
+            }
 
             Console.ReadKey();
-
         }
 
 
+
+        //int luckyNumber;
+        //Console.WriteLine("Введите шестизначное число:");
+
+        //if (!int.TryParse(Console.ReadLine(), out luckyNumber))
+        //{
+        //    Console.WriteLine("Введено не число!");
+        //    Console.ReadKey();
+        //    return;
+        //}
+
+        //int luckyNumber1 = luckyNumber / 100000;
+        //int luckyNumber2 = luckyNumber / 10000 % 10;
+        //int luckyNumber3 = luckyNumber / 1000 % 10;
+        //int luckyNumber4 = luckyNumber / 100 % 10;
+        //int luckyNumber5 = luckyNumber / 10 % 10;
+        //int luckyNumber6 = luckyNumber % 10;
+
+        //if (luckyNumber1 + luckyNumber2 + luckyNumber3 == luckyNumber4 + luckyNumber5 + luckyNumber6)
+        //{
+        //    Console.WriteLine($"Билет с номером {luckyNumber} счастливый!");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"Билет с номером {luckyNumber} несчастливый");
+        //}
     }
 }
