321127032469e027189a5d5495ed84f0980ab8f7
Fancy <lev2001504@gmail.com>
2023-09-16 11:04:08 +0200

    Some LINQ examples

---
+++ LINQ-Experiments/LINQ-Experiments/Furry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Experiments
+{
+    public class Furry
+    {
+        public string Name { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public int Yiffyness { get; set; }
+    }
+}
--- LINQ-Experiments/LINQ-Experiments/Program.cs
+++ LINQ-Experiments/LINQ-Experiments/Program.cs
@@ -1,10 +1,177 @@
 ﻿namespace LINQ_Experiments
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static IReadOnlyCollection<Furry> _furries = new List<Furry>()
         {
-            Console.WriteLine("Hello, World!");
+            // Создаём объект класса Furry и сразу задаём его свойства (конструктора у Furry нет)
+            new Furry()
+            {
+                Name = "Howler",
+                Sex = Sex.Male,
+                Yiffyness = 30
+            },
+
+            new Furry()
+            {
+                Name = "Goldfur",
+                Sex = Sex.Herm,
+                Yiffyness = 100
+            },
+
+            new Furry()
+            {
+                Name = "Lozi",
+                Sex = Sex.Male,
+                Yiffyness = 90
+            },
+
+            new Furry()
+            {
+                Name = "Jarvvitch",
+                Sex = Sex.Female,
+                Yiffyness = 40
+            },
+
+            new Furry()
+            {
+                Name = "Varra",
+                Sex = Sex.Female,
+                Yiffyness = 10
+            },
+
+            new Furry()
+            {
+                Name = "Varra Male",
+                Sex = Sex.Male,
+                Yiffyness = 10
+            }
+        };
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Исходные фурри:");
+            PrintFurries(_furries);
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintMaleFurries();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintFurriesCount();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintFurriesOrderedByYiffynessDesc();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintFurriesWithLInName();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintTwoMostYiffyFurries();
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            PrintFemalesWithVarInName();
+
+            Console.ReadLine();
+        }
+
+        #region Здесь методы - задания, на одно задание по методу
+
+        /// <summary>
+        /// Выделить самцов
+        /// </summary>
+        private static void PrintMaleFurries()
+        {
+            Console.WriteLine("Самцы:");
+
+            var maleFurries = _furries
+                .Where(f => f.Sex == Sex.Male)
+                .ToList();
+
+            PrintFurries(maleFurries);
+        }
+
+        /// <summary>
+        /// Посчитать число фуррей
+        /// </summary>
+        private static void PrintFurriesCount()
+        {
+            Console.WriteLine("Число фуррей:");
+
+            var furriesCount = _furries
+                .Count();
+
+            Console.WriteLine(furriesCount);
+        }
+
+        /// <summary>
+        /// Напечатать отсортированных по убыванию йиффливости фуррей
+        /// </summary>
+        private static void PrintFurriesOrderedByYiffynessDesc()
+        {
+            Console.WriteLine("Фурри по убыванию йиффливости");
+
+            var orderedFurries = _furries
+                .OrderByDescending(f => f.Yiffyness)
+                .ToList();
+
+            PrintFurries(orderedFurries);
+        }
+
+        /// <summary>
+        /// Напечатать фуррей с буквой "l" в имени (маленькая l, не большая)
+        /// </summary>
+        private static void PrintFurriesWithLInName()
+        {
+            Console.WriteLine("Фурри с буквой l в имени");
+
+            var furriesWithL = _furries
+                .Where(f => f.Name.Contains("l"))
+                .ToList();
+
+            PrintFurries(furriesWithL);
+        }
+
+        /// <summary>
+        /// Напечатать двух самых йиффливых фуррей
+        /// </summary>
+        private static void PrintTwoMostYiffyFurries()
+        {
+            Console.WriteLine("Два самых йиффливых фурря");
+
+            var twoYiffiestFurries = _furries
+                .OrderByDescending(f => f.Yiffyness)
+                .Take(2)
+                .ToList();
+
+            PrintFurries(twoYiffiestFurries);
+        }
+
+        /// <summary>
+        /// Напечатать всех самок с буквосочетанием Var в имени
+        /// </summary>
+        private static void PrintFemalesWithVarInName()
+        {
+            Console.WriteLine("Самки с Var в имени");
+
+            var femalesWithVarInName = _furries
+                .Where(f => f.Sex == Sex.Female)
+                .Where(f => f.Name.Contains("Var"))
+                .ToList();
+
+            PrintFurries(femalesWithVarInName);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Метод, распечатывающий фуррей
+        /// </summary>
+        private static void PrintFurries(IReadOnlyCollection<Furry> furries)
+        {
+            foreach(var furry in furries)
+            {
+                Console.WriteLine($"Имя: { furry.Name }, Пол: { furry.Sex }, Йиффливость: { furry.Yiffyness }");
+            }
         }
     }
 }
