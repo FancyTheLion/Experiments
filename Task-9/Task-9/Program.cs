68d4908c0122ae160611e6f5d65b1478063882d6
Fancy <lev2001504@gmail.com>
2023-09-19 16:15:54 +0200

    Осознанно осваиваю swich

---
+++ Task-9/Task-9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца от 1 до 5:");
+
+            int.TryParse(Console.ReadLine(), out int month);
+
+
+                switch (month) // думал как ввести проверку. не смог :( 
+                {
+                    case 1: Console.WriteLine($"Количество дней в {month} месяце: 31"); break;
+                    case 2: Console.WriteLine($"Количество дней в {month} месяце: 30"); break;
+                    case 3: Console.WriteLine($"Количество дней в {month} месяце: 29"); break;
+                    case 4: Console.WriteLine($"Количество дней в {month} месяце: 28"); break;
+                    case 5: Console.WriteLine($"Количество дней в {month} месяце: 28()"); break;
+                    default: Console.WriteLine("Не корректный ввод"); break;
+                }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
