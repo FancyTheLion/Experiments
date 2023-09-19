770fc3b829042d348e1edcb8467381fc4911293f
Fancy <lev2001504@gmail.com>
2023-09-19 18:48:21 +0200

    Проверка числа на четность

---
+++ Test-10/Test-10/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Test_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите максимальное четное число для отображения каждого четного числа:");
+
+            int.TryParse(Console.ReadLine(), out int numberEven);
+
+            for (int i = 1; i <= numberEven; i++) 
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+
+            //int.TryParse(Console.ReadLine(), out int numberEven);
+
+            //for (int i = 2;  i <= numberEven; i = i + 2)
+            //{
+            //    Console.WriteLine($"Число {i} четное!");
+            //}
+        }
+    }
+}
