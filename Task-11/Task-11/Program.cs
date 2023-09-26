77e10997a605228114db05ba3923788f4a3f56fb
Fancy <lev2001504@gmail.com>
2023-09-26 12:04:11 +0200

    Одновременно запущены 2 метода

---
+++ Task-11/Task-11/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_11
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Task.Run(AsyncValueCycleFive);
+
+            Task.Run (AsyncValueCycleTen);
+
+            Console.ReadLine();
+
+        }
+
+        /// <summary>
+        /// Отображение каждого n + 5 числа 
+        /// </summary>
+        public static async Task AsyncValueCycleFive()
+        {
+            for (int i = 0; true; i += 5)
+            {
+                Console.WriteLine($"Число а: {i}");
+
+                await Task.Delay(500);
+            }
+        }
+
+        /// <summary>
+        /// Отображение каждого n + 10 числа 
+        /// </summary>
+        public static async Task AsyncValueCycleTen()
+        {
+
+            for (int i = 0; true; i += 10)
+            {
+                Console.WriteLine($"Число b: {i}");
+
+                await Task.Delay(700);
+            }
+        }
+
+    }
+
+
+
+}
