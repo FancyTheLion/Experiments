51e45193e2c957aec51ec8a1ef38898c561306ed
Fancy <lev2001504@gmail.com>
2023-09-26 15:54:07 +0200

    Подкручены методы Task

---
+++ Task-12(Upgrade Task-11)/Task-12(Upgrade Task-11)/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_12_Upgrade_Task_11_
+{
+    internal class Program
+    {
+        #region Ограничения длительности циклов
+
+        private const int LimitationCycle_1 = 50;
+        private const int LimitationCycle_2 = 100;
+
+        #endregion
+        async static Task Main(string[] args)
+        {
+            Task.WaitAll(AsyncValueCycleFive(), AsyncValueCycleTen());
+
+        }
+
+        /// <summary>
+        /// Отображение n + 5 чисел
+        /// </summary>
+        public static async Task AsyncValueCycleFive()
+        {
+            for (int i = 0; i <= LimitationCycle_1; i += 5)
+            {
+                Console.WriteLine($"Число а: {i}");
+
+                await Task.Delay(300);
+            }
+        }
+
+        /// <summary>
+        /// Отображение n + 10 чисел
+        /// </summary>
+        public static async Task AsyncValueCycleTen()
+        {
+            for (int i = 0; i <= LimitationCycle_2; i += 10)
+            {
+                Console.WriteLine($"Число b: {i}");
+
+                await Task.Delay(300);
+            }
+        }
+
+    }
+}
