166ca8d933c776d145edfec823226e4fa84bb1b1
Fancy <lev2001504@gmail.com>
2023-09-28 15:18:08 +0200

    На Json сделана сериализация и десериализация с котами

---
+++ Task-13/Task-13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Text.Encodings.Web;
+using System;
+
+namespace Task_13
+{
+    public class BigCat
+    {
+        public string PantheraGenus { get; set; }
+        public string Predator { get; set; }
+        public int AverageLifeExpectancy { get; set; }
+    }
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BigCat bigCat = new BigCat()
+            {
+                PantheraGenus = "Лев",
+                Predator = "Хищное млекопитающее",
+                AverageLifeExpectancy = 15
+            };
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+
+            string jsonStringBigCat = JsonSerializer.Serialize(bigCat, options);
+
+            Console.WriteLine(jsonStringBigCat);
+
+            BigCat restoredBigCat = JsonSerializer.Deserialize<BigCat>(jsonStringBigCat);
+
+            Console.WriteLine(restoredBigCat.PantheraGenus);
+            Console.WriteLine(restoredBigCat.Predator);
+            Console.WriteLine(restoredBigCat.AverageLifeExpectancy);
+
+            Console.ReadLine();
+        }
+    }
+}
