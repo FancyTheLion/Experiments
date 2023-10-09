4461362a6ef51c72bb11ab105fb4cb29d3555a60
FancyTheLion <lev2001504@gmail.com>
2023-10-09 14:00:02 +0200

    Осознанно написал возведение в степень

---
+++ Task-16/Task-16/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(GetSquare(42));
+
+            Console.ReadKey();
+        }
+
+
+        private static double GetSquare(double t)
+        {
+            double a;
+
+            return a = Math.Pow(t, 2);
+        }
+
+        private static void Print(double t)
+        {
+            Console.WriteLine(t);
+        }
+    }
+}
