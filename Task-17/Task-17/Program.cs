318a7022cacbfa443bd9bd86c0816939c6bd7ded
FancyTheLion <lev2001504@gmail.com>
2023-10-09 15:26:33 +0200

    Программа получает строку, убирает половину символов, убивает пробелы.

---
+++ Task-17/Task-17/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLastHalf("I love CSharp  !"));
+
+            Console.ReadKey();
+        }
+
+        public static string GetLastHalf(string text)
+        {
+            text = text.Substring(text.Length/2);
+            text = text.Replace(" ", null); //А как корректнее? Так или через Empty
+            return text;
+        }
+    }
+}
