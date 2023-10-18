8f05a4c9f7fbe0f1f79dfaf6059d48ba928d35cd
FancyTheLion <lev2001504@gmail.com>
2023-10-18 14:31:26 +0200

    Шахматы

---
+++ Task-22/Task-22/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsCorrectMove("a1", "d4");
+            IsCorrectMove("f4", "e7");
+            IsCorrectMove("a1", "a4");
+
+            Console.ReadKey();
+        }
+
+        public static bool IsCorrectMove(string from, string to)
+        {
+            var dx = Math.Abs(to[0] - from[0]); //смещение фигуры по горизонтали
+            var dy = Math.Abs(to[1] - from[1]); //смещение фигуры по вертикали
+
+            return (dx != 0 || dy != 0) && (dx == dy || dx * dy == 0);
+        }
+    }
+}
