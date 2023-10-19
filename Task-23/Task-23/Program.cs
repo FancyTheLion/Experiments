f99dc002f6e38a3e8e74cec55c972ac6f3799617
FancyTheLion <lev2001504@gmail.com>
2023-10-19 17:38:07 +0200

    Robot and Boss

---
+++ Task-23/Task-23/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(ShouldFire(true, "boss", 79));
+            Console.WriteLine(ShouldFire2(true, "boss", 44));
+
+            Console.ReadKey();
+        }
+
+        public static bool ShouldFire(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            bool shouldFire = true;
+            if (enemyInFront == true)
+            {
+                if (enemyName == "boss")
+                {
+                    if (robotHealth < 50) shouldFire = false;
+                    if (robotHealth > 100) shouldFire = true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return shouldFire;
+        }
+
+        public static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
+        {
+            return ((enemyInFront == true) && (enemyName == "boss") && (robotHealth >= 50)) || ((enemyInFront == true) && (enemyName != "boss"));
+        }
+    }
+}
