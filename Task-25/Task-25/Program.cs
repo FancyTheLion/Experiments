318682c73f5d929b144f641fe2941bf0200fb615
FancyTheLion <lev2001504@gmail.com>
2023-10-20 11:03:04 +0200

    Задача с степенями

---
+++ Task-25/Task-25/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(2)); // => 4
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(15)); // => 16
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(-2)); // => 1
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(-100));
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(100));
+
+            Console.ReadKey();
+        }
+
+        private static int GetMinPowerOfTwoLargerThan(int number)
+        {
+            int result = 1;
+            while (result <= number)
+            result *= 2;
+            return result;
+        }
+    }
+}
