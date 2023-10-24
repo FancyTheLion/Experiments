3c378cd3b928fb7939572f7d4da742be754cac17
FancyTheLion <lev2001504@gmail.com>
2023-10-24 18:06:22 +0200

    For и Рамочки (:

---
+++ Task-27/Task-27/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_27
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteTextWithBorder("Menu:");
+            WriteTextWithBorder("");
+            WriteTextWithBorder(" ");
+            WriteTextWithBorder("Game Over!");
+            WriteTextWithBorder("Select level:");
+        }
+
+        private static void WriteTextWithBorder(string text)
+        {
+            for (int i = 0; i < 1; i++) Console.Write("+");
+            for (int j = 1; j <= text.Length + 2; j++) Console.Write("-");
+            for (int i = 0; i < 1; i++) Console.Write("+\n" + "| " + text + " |");
+            for (int i = 0; i < 1; i++) Console.Write("\n+");
+            for (int j = 1; j <= text.Length + 2; j++) Console.Write("-");
+            for (int i = 0; i < 1; i++) Console.Write("+\n");
+        }
+    }
+}
