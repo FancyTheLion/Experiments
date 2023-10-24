29ce5536e300ca9fd15993b9f6233e34c0d40493
FancyTheLion <lev2001504@gmail.com>
2023-10-24 18:50:42 +0200

    Рисуем шахматную доску из "#" и "."

---
+++ Task-28/Task-28/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteBoard(8);
+            WriteBoard(1);
+            WriteBoard(2);
+            WriteBoard(3);
+            WriteBoard(10);
+            
+            Console.ReadKey();
+        }
+
+        private static void WriteBoard(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if ((j + i) % 2 == 0)
+                        Console.Write("#");
+                    else Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
