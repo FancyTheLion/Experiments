10842f0cf5e38fdba3f748f690f249f0af1e3b1b
FancyTheLion <lev2001504@gmail.com>
2023-10-25 09:48:43 +0200

    Массив и вовод четных чисел. Недоделан вывод на консоль :(

---
+++ Task-29/Task-29/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_29
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            foreach (var item in GetFirstEvenNumbers(count))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static int[] GetFirstEvenNumbers(int count)
+        {
+            int[] array = new int[count];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (i + 1) * 2;
+            }
+            return array;
+        }
+    }
+}
