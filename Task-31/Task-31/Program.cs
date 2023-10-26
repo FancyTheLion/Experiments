17e205305ab796105082720a2e012065c768877d
FancyTheLion <lev2001504@gmail.com>
2023-10-26 13:50:42 +0200

    Отображение позиции максимального числа в массиве

---
+++ Task-31/Task-31/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = {1, 5, 20, 40};
+
+            Console.WriteLine(MaxIndex(a));
+
+            Console.ReadKey();
+        }
+
+        public static double MaxIndex(double[] array)
+        {
+            double maxI = 0;
+            double maxValue = double.MinValue;
+            double i = 0;
+            foreach (var item in array)
+            {
+                if (item > maxValue)
+                {
+                    maxValue = item;
+                    maxI = i;
+                }
+                i++;
+            }
+            if (array.Length == 0)
+            {
+                return -1;
+            }
+            return maxI;
+        }
+    }
+}
