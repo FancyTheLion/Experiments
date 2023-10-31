fbd93b18968d9c66a19a9b51f50879e968c5af51
FancyTheLion <lev2001504@gmail.com>
2023-10-31 11:37:42 +0100

    Carts Taro

---
+++ Task-34/Task-34/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_34
+{
+    public class Program
+    {
+        enum Suits
+        {
+            Wands,
+            Coins,
+            Cups,
+            Swords
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSuit(Suits.Wands));
+            Console.WriteLine(GetSuit(Suits.Coins));
+            Console.WriteLine(GetSuit(Suits.Cups));
+            Console.WriteLine(GetSuit(Suits.Swords));
+
+            Console.ReadLine();
+        }
+
+        private static string GetSuit(Suits suit)
+        {
+            return suit == Suits.Swords ? "мечей" : (suit == Suits.Coins ? "монет" : (suit == Suits.Cups ? "кубков" : "жезлов"));
+        }
+    }
+}
